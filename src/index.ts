e8bb0a0b504661f31df2962f9115884502a35b6e
DESKTOP-J6GHQ5I\우경정보기술 <jwyou@wkit.co.kr>
2023-12-22 14:17:17 +0900

    merge cron job to index

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,43 @@ import 'dotenv/config'
 import { sendKakaoLink } from './utils/kakaoLink';
 import { kalinkCharacterData } from './types/messageTemplate';
 import { RoomCacher } from './service/roomCaching';
+import { persistMarketData } from './utils/axiosLostarkApi';
+import { scheduleJob } from 'node-schedule';
+import { KakaoSession } from './utils/KakaoSession';
+
+//initalizing scheduling
+scheduleJob("stuffApiCrawl", '0 * * * *', async ()=>{
+    console.log((new Date()).toLocaleString())
+    console.log("item scrap start");
+    await persistMarketData(90000);
+    await persistMarketData(70000);
+    await persistMarketData(60000);
+    await persistMarketData(50000);
+    await persistMarketData(40000, '전설');
+})
+
+scheduleJob("kakaoSession", '*/10 * * * *', async ()=>{
+    console.log((new Date()).toLocaleString())
+    console.log("renew kakao session");
+    const session = new KakaoSession();
+    await session.init();
+
+    let isLogin = await session.checkCurrentCookie();
+    if(isLogin){
+        console.log("logged in.")
+    }else{
+        console.log("session expired.")
+        await session.setKakaoSession(process.env['kakao_id'] as string, process.env['kakao_password'] as string);
+    }
+    await session.desctruct();
+
+    return;
+
+})
+
+
+
+
 
 //const cookie: string = '';
 const appKey: string = '66dd278aa59de80b4f87c6123a0e51c1';
